efc2574bc78888c45aa4ed2a9cd77dbad1af88c8
Toby Penk <tobypenk@gmail.com>
2020-12-15 09:07:41 -0500

    Add about page

---
+++ about/index.php
@@ -0,0 +1,311 @@
+<?php
+  
+  //$recipe_name = basename(__DIR__);
+  //include("../scripts/php/auto_recipe.php");
+  
+?>
+
+<html lang="en">
+  <head>
+
+  	
+  	<?php 
+      include "../old_site/templates/head.html" 
+    ?>
+    
+    <link rel='stylesheet' href='../css/main.css' />
+    <link href="https://fonts.googleapis.com/css?family=Didact+Gothic|Reem+Kufi|Source+Sans+Pro:200,400,700" rel="stylesheet">
+	<script src="../js/jquerylib.js"></script>
+
+  </head>
+  <body>
+    
+    <?php 
+      include "../html_templates/header.html";
+      
+    ?>
+    
+    <section class="about-content-area content-area logged-out" id="about-content-area">
+    <div class="about-display" id="about-display">
+        <h1 class="header-bar">ABOUT RAASIPE</h1>
+        
+        <!--
+        <p class="content-paragraph">
+            <span class="background-color">Reciprocity</span> is not a recipe website. It's a tool designed to
+            help you effortlessly create unforgettable culinary experiences.
+        </p>
+        <div class="clear"></div>
+        -->
+
+        <img class="img-float-left" src="../old_site/assets/images/tp_comp.jpeg" width="225px" height="300px" alt="Moments before his arrest by Roman centurions on charges of precipitating the Catiline conspiracy" />
+        <p class="content-paragraph">
+            RaaSipe is the project of Phinneasticus T. Barchbellow, known to the IRS as "Toby Penk." At left is the only known image of Barchbellow, taken moments before his historic arrest by Ottoman officials for conspiring with Gavrilo Princip to plunge the civilized world into existential conflict.
+        </p>
+
+        <div class="clear"></div>
+
+        <h2 class="underline">A Respite from Long, Pointless Stories</h2>
+        <p class="content-paragraph">
+            If I wanted to hear you pontificate on the virtues of cashew- vs. almond-butter, I'd attend your one-woman show in Portland. For now, I just want a recipe, and I don't want to scroll past seventy-five pages of rambling diatribe / rhapsody / fever-dream to finally get to the actual recipe and learn that it sucks. I also don't want dozens of high-resolution images that add even more inches of scrolling and make the actual recipe jump around as the images load in fits and starts on my crappy internet connection. Let me make my own stories, and let me make my own images. The real-life kind of images. You know, actual food.
+        </p>
+
+        <div class="clear"></div>
+
+        <h2 class="underline">The Tyranny of SEOppressive Search Engines</h2>
+        <p class="content-paragraph">
+            I don't blame the aspiring blogopracter for employing these methods; in fact, it's crazy not to, because if you don't, you'll be as invisible to search engines as a 9th-grade chess champion is to its dating prospects. All those words are the equivalent of seduction, search-engine style. Come on, babe, I'm totally relevant. No, I would never trick people into reading pointless drivel just by including paragraph upon paragraph of blatantly pandering keywords. I'm genuine. I'm relatable. I'm an influencer, whatever that means. Just gimme a chance to serve up that sweet, sweet content.
+        </p>
+
+        <div class="clear"></div>
+
+        <h2 class="underline">Well, What's so Great About YOUR Stupid Website, Smart Guy?</h2>
+        <p class="content-paragraph">
+            Wow. I thought we'd built up some rapport over the last couple paragraphs, but I suppose you set higher standards than the average search engine. I guess I respect that.
+        </p>
+
+        <p class="content-paragraph">
+            My goal with RaaSipe is to ameliorate a(n) historical accident: the fact that the recipe was invented well before the computer. If we invented recipes today, they'd be incredible, multi-functional works of technological art. You'd be able to
+            
+            <br>
+            <br>
+            - Size the calorie, fat, carb, and protein contents of any recipe, based on the ingredients.</li>
+            <br>
+            - Change the quantity of any ingredient and have the recipe change the quantities of everything else in proportion.
+            <br>
+            - Substitute ingredients and see the effect that would have on the nutrition of the recipe.</li>
+            <br>
+            - Consolidate grocery lists across multiple recipes and text them to yourself to take to the store.
+            <br>
+            - Exclude recipes that require equipment you don't own, and use the recipe as a link to get the proper equipment.
+            <br>
+            - Exclude recipes that violate your dietary restrictions, and use the recipe to find substitutes that will help you keep kosher (or vegetarian, or vegan, or etc etc).
+            <br>
+            - Cook any recipe, no matter how "fancy," because it would be presented in a way that makes it foolproof (I apologize for insinuating that you're a fool - it's just a word).
+            <br>
+            <br>
+            As a result, you'd learn to cook faster and better, and there would be no such thing as a difficult recipe. After all, a recipe is nothing but a list of inputs and a list of instructions. Even puppies can follow instructions. Are you stupider than a puppy?
+        </p>
+        <div class="clear"></div>
+
+        <p class="content-paragraph">
+            I didn't think so.
+        </p>
+        <div class="clear"></div>
+
+        <h2 class="underline">Quit Thee this Farce, and Hark! To the Great Fray!</h2>
+        <p class="content-paragraph">
+            Well, you made it. Reading this introduction qualifies you to cook any of the recipes in our catalog, and indemnifies us against any liability for injury, malfeasance, embezzlement, capital murder, high treason, or crossing of the Rubicon with armed forces in peacetime, caused by use or non-use of this site, both on- and off-line, in this location and all locations, for all future time and retroactively to the beginning of the universe, regardless of all mutations to the laws of physics during the first critical nanoseconds of the big bang.
+        </p>
+        <div class="clear"></div>
+        <p class="content-paragraph">
+            When you apply heat and time to food, you're doing something people have done since there have been people, and something that will make your life a little more beautiful. It's a not-that-hard way to share your humanity with others, and it's fun, even when you burn yourself or accidentally cook things that are disgusting. So stop reading this stupid article and hit the kitchen. Go! Before I change my mind!
+        </p>
+
+        <div class="clear"></div>
+        <div class="clear"></div>
+        <div class="clear"></div>
+        <div class="clear"></div>
+
+        <!--
+        <div class="clear"></div>
+        <h2 class="underline">Ease of Use</h2>
+        <p class="content-paragraph">
+            The recipes on <span class="background-color">Reciprocity</span> are smarter than the ones you're
+            used to. If you need to double the recipe</p>
+            <div class="inline-icon double-recipe"></div>
+            <p class="content-paragraph">or cut it in half</p>
+            <div class="inline-icon halve-recipe"></div>
+            <p class="content-paragraph">, there are simple buttons for that.</p>
+
+        <div class="clear"></div>
+        <p class="content-paragraph">
+            Want to serve 7 guests, but you have a recipe that serves 4? Does a recipe call for 6 tablespoons of
+            butter, but you
+            only have 5? Just click the number next to "serves" or the number next to "butter" in the recipe and
+            add whatever number fits your needs. The recipe will adjust all of the other quantities to fit.
+        </p>
+        <div class="clear"></div>
+        <h2 class="underline">Fit With Your Life</h2>
+        <p class="content-paragraph">
+            When it comes time to print the recipe, just click the print icon</p>
+            <div class="inline-icon print"></div>
+            <p class="content-paragraph">and <span class="background-color">Reciprocity</span> will present a
+            beautifully formatted page to print, with plenty of space for notes, and no huge color images,
+            advertisements, or other web-confetti. You'll want to keep the recipes in a binder of your own as you
+            build up a personal or family recipe book.</p>
+            
+        <div class="clear"></div>
+        <p class="content-paragraph">
+            Does a recipe call for Worcestershire Sauce, but you just ran out? Just click "Worcestershire Sauce"
+            in the ingredients list, and <span class="background-color">Reciprocity</span> will suggest alternatives.
+            (soy sauce, anyone?)
+        </p>
+        <div class="clear"></div>
+        <p class="content-paragraph">
+            Are you a vegetarian? Vegan? Do you need to keep gluten-free, sugar-free, dairy-free, nut-free, alcohol-free,
+            or avoid shellfish? <span class="background-color">Reciprocity</span> not only helps you find recipes that fit
+            your needs and preferences, but will also help you adapt existing recipes to your dietary needs by offering
+            substitutes for ingredients you'd rather steer clear of. You can easily see which recipes are and aren't going
+            to work for your diet with our badges:</p>
+            <div class="inline-icon vegetarian"></div>
+            <div class="inline-icon vegan"></div>
+            <div class="inline-icon gluten_free"></div>
+            <div class="inline-icon sugar_free"></div>
+            <div class="inline-icon alcohol_free"></div>
+            <div class="inline-icon dairy_free"></div>
+            <div class="inline-icon nut_free"></div>
+            <div class="inline-icon shellfish_free"></div>
+        
+        <div class="clear"></div>
+        <h2 class="underline">Out of the Kitchen</h2>
+        <p class="content-paragraph">
+            Planning a dinner party, or meals for the week? <span class="background-color">Reciprocity</span>'s meal
+            planner</p>
+            <div class="inline-icon add-recipe"></div>
+            <p class="content-paragraph">aggregates the recipes you've chosen, and creates a grocery list for you.
+            You'll even see whether your whole menu fits your dietary concerns.</p>
+        <div class="clear"></div>
+        <p class="content-paragraph">
+            When it's time to go shopping, <span class="background-color">Reciprocity</span> will text</p>
+            <div class="inline-icon text"></div>
+            <p class="content-paragraph">or email</p>
+            <div class="inline-icon email"></div>
+            <p class="content-paragraph">you your grocery list so that you can have it on hand in the store - alternatively, you can</p>
+            <div class="inline-icon print"></div>
+            <p class="content-paragraph">print the grocery list and bring it with you.</p>
+            
+        <div class="clear"></div>
+        <p class="content-paragraph">
+            Have you ever started a recipe, only to find out halfway through that it requires some piece of kitchen
+            equipment</p>
+            <div class="inline-icon kitchen-torch"></div>
+            <p class="content-paragraph">you don't have on hand? <span class="background-color">Reciprocity</span> helps you understand the
+            equipment requirements of every recipe, so that you'll always be prepared and can't be caught off-guard by
+            some fancy piece of gadgetry.
+            <div class="inline-icon food-processor"></div>
+        </p>
+        <div class="clear"></div>
+        <h2 class="underline">Make it Your Own</h2>
+        <p class="content-paragraph">
+            As you grow in your culinary adventure with <span class="background-color">Reciprocity</span>, it will learn and
+            remember which staple ingredients you have, what your dietary preferences are, and what kitchen equipment
+            you have available. We will start to proactively provide suggestions for new dishes to try, and the better
+            we get to know you, the better our recommendations will be. We're here to make this journey as easy and
+            fun as possible, and our tools are designed to help you master every technique, from whisking egg yolks to
+            piping macarons.
+        </p>
+        <div class="clear"></div>
+    -->
+        <p class="content-paragraph">
+            If you've found a bug of some sort, or if you want some new functionality, please tell me. I promise that I will personally respond, doing my miniscule part to repair your mostly-accurate perception that these types of website feedback tools are mere venting stations to unmonitored email inboxes in databases buried deep within the Earth.
+        </p>
+    
+        <section class="contact-boat">
+            <div class="contact-options">
+                <h2>Contact About:</h2>
+                <div class="contact-option" data-subject-association="I have a suggestion for an improvement">
+                    <p class="contact-option-text">Suggestion</p>
+                </div>
+                <div class="contact-option" data-subject-association="I found a bug!">
+                    <p class="contact-option-text">Bug Fix</p>
+                </div>
+                <div class="contact-option" data-subject-association="General stuff">
+                    <p class="contact-option-text">Question</p>
+                </div>
+                <div class="contact-option" data-subject-association="I'd like to request information">
+                    <p class="contact-option-text">Info</p>
+                </div>
+            </div>
+            <div class="contact-form">
+                <h2>Your Info:</h2>
+                <div class="input-holder">
+                    <input class="contact-input" type="text" id="name" placeholder="" required="required"/>
+                    <p class="input-title">Your Name</p>
+                </div>
+                <div class="input-holder">
+                    <input class="contact-input" type="text" id="email" placeholder="" required="required"/>
+                    <p class="input-title">Your Email</p>
+                </div>
+                <div class="input-holder">
+                    <input class="contact-input" type="text" id="subject" placeholder="" required="required"/>
+                    <p class="input-title">Message Subject</p>
+                </div>
+                <div class="input-holder textarea">
+                    <textarea class="contact-input" type="text" id="message" placeholder="" required="required"></textarea>
+                    <p class="input-title">Message</p>
+                </div>
+                <div class="input-holder contact-submit" id="contact-submit">
+                    <div class="submit-button-contact" id="submit"></div>
+                    <p class="input-title" id="submit-title">Submit</p>
+                </div>
+            </div>
+        </section>
+    </div>
+</section>
+
+<script>
+	$(document).on("click touch","#contact-submit",function() {
+	    $(".contact-input:not(:valid)").parent().addClass("invalid-input");
+	    
+	    var name = $("#name").val().trim(),
+	        email = $("#email").val().trim(),
+	        subject = $("#subject").val().trim(),
+	        message = $("#message").val().trim(),
+	        error_message = "";
+	        
+	    if (name == "" || email == "" || subject == "" || message == "") {
+	        $("#submit-title").html("I need some more info.");
+	        return false;
+	    } else if (!validate_email(email)) {
+	        $("#email").parent().addClass("invalid-input");
+	        $("#submit-title").html("Please provide a valid email address.");
+	        return false;
+	    } else {
+	        $("#contact-submit").addClass("submitting");
+	        $("#submit-title").html("Working...");
+	        
+	        $.ajax({
+	            url: init_path + "php/services/send_api_email.php",
+	            method: "POST",
+	            data: {
+	                user_name: name + " " + email,
+	                recipient: "tobypenk@gmail.com",
+	                subject: subject,
+	                html_message: message,
+	                plain_message: message
+	            },
+	            success: function(e) {
+	                console.log(e);
+	                if (e.match("Message has been sent")) {
+	                    $("#contact-submit").addClass("submitted");
+	                    $("#contact-submit").removeClass("submitting");
+	                    $("#submit-title").html("Success!! I'll get back to you shortly.");
+	                } else {
+	                    $("#contact-submit").addClass("failed");
+	                    $("#contact-submit").removeClass("submitting");
+	                    $("#submit-title").html("Message failed!! Give it another shot.");
+	                }
+	            },
+	            error: function() {
+	                $("#contact-submit").addClass("failed");
+	                $("#contact-submit").removeClass("submitting");
+	                $("#submit-title").html("Message failed!! Give it another shot.");
+	            }
+	        });
+	    }
+	});
+
+</script>
+
+
+
+    <section id="home-content-area"></section>
+    <section id="palette-content-area"></section>
+  </body>
+</html>
+
+
+
+
+
+
