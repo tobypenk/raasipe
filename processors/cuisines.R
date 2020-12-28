1700b2b3341bd1fd173d6723cbd63678e5a85b55
Toby Penk <tobypenk@gmail.com>
2020-12-28 17:01:09 -0500

    Place of origin prediction RNN from ordered ingredient list

---
+++ processors/cuisines.R
@@ -0,0 +1,47 @@
+setwd("/Applications/MAMP/htdocs/raasipe/processors")
+source("process.R")
+
+cuisines_map <- c(
+  "spanish"="europe",
+  "american"="americas",
+  "me"="westasia",
+  "ancient"="ancient",
+  "french"="europe",
+  "indian"="westasia",
+  "mexican"="americas",
+  "english"="europe",
+  "italian"="europe",
+  "filipino"="eastasia",
+  "german"="europe",
+  "greek"="europe",
+  "african"="africa",
+  "vietnamese"="eastasia",
+  "japanese"="eastasia",
+  "russian"="europe",
+  "serbian"="europe",
+  "portugal"="europe",
+  "sa"="americas",
+  "chinese"="eastasia",
+  "korean"="eastasia",
+  "polish"="europe"
+)
+
+recipes <- execute_query(paste0(
+  " select ",
+  " replace(replace(r.name,'%20',' '),'%27','') recipe, ",
+  " r.origin cuisine, ",
+  " replace(replace(replace(replace(i.singular_name,'%20',''),'%27',''),'%E7','c'),'+','') ingredient ",
+  " from recipe r join step s on s.recipe_id = r.id ",
+  " join material m on m.step_id = s.id",
+  " join ingredient i on m.ingredient_id = i.id",
+  " where origin is not null and origin not in ('ancient');"
+))
+
+df <- data.frame(cuisine=c(1),ingredient_string=c(1))
+for (r in unique(recipes$recipe)) {
+  tmp_df <- subset(recipes,recipe==r)
+  tmp_df$cuisine = cuisines_map[tmp_df$cuisine]
+  df <- rbind(df,c(tmp_df[1,"cuisine"],paste0(tmp_df$ingredient,collapse=" ")))
+}
+
+write.csv(df[2:nrow(df),],"cuisines.csv",row.names = FALSE)
---
+++ processors/cuisines.py
@@ -0,0 +1,96 @@
+import csv
+import tensorflow as tf
+import numpy as np
+import os
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+print(os.getcwd())
+vocab_size = 800
+embedding_dim = 32
+max_length = 50
+trunc_type='post'
+padding_type='post'
+oov_tok = "<OOV>"
+training_portion = .7
+
+sentences = []
+labels = []
+stopwords = [ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ]
+
+with open("cuisines.csv", 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    next(reader)
+    for row in reader:
+        labels.append(row[0])
+        sentences.append(" ".join([word for word in row[1].split() if word not in stopwords]))
+
+train_size = int(len(sentences) * training_portion)
+
+train_sentences = sentences[:train_size]
+train_labels = labels[:train_size]
+
+validation_sentences = sentences[train_size:]
+validation_labels = labels[train_size:]
+
+tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(train_sentences)
+word_index = tokenizer.word_index
+
+train_sequences = tokenizer.texts_to_sequences(train_sentences)
+train_padded = pad_sequences(train_sequences, padding=padding_type, maxlen=max_length)
+
+validation_sequences = tokenizer.texts_to_sequences(validation_sentences)
+validation_padded = pad_sequences(validation_sequences, padding=padding_type, maxlen=max_length)
+
+label_tokenizer = Tokenizer()
+label_tokenizer.fit_on_texts(labels)
+
+training_label_seq = np.array(label_tokenizer.texts_to_sequences(train_labels))
+validation_label_seq = np.array(label_tokenizer.texts_to_sequences(validation_labels))
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(6, activation='softmax')
+])
+model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+num_epochs = 40
+
+history = model.fit(
+    train_padded, 
+    training_label_seq, 
+    epochs=num_epochs, 
+    validation_data=(
+        validation_padded, 
+        validation_label_seq
+    )
+)
+
+
+
+
+import matplotlib.pyplot as plt
+
+
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+  
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")
+
+
+
+
+
+
+
+
+
+
