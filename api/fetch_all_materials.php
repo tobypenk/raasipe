65a9fbbf98fd0842c0e6a56c85eebfda95a0ba2c
Toby Penk <tobypenk@gmail.com>
2021-01-13 21:28:37 -0500

    Update material shortcut script

--- api/fetch_all_materials.php
+++ api/fetch_all_materials.php
@@ -18,7 +18,10 @@
 	
 	
 	$total = [];
+	$t_str = [];
+	$t_tok = [];
 	
+/*
 	foreach($m as $i) {
 		$ti = [];
 		$comma = (is_null($i["preparation"]) | trim($i["preparation"]) == "") ? "" : ",";
@@ -29,6 +32,75 @@
 		
 		array_push($total,trim(implode(" ", $ti)));
 	}
+*/
+	
+	// quantity = 0, measure = 1, type = 2, ingredient = 3, prep = 4
+	// could potentially add additional category for measure type; e.g. 'small clove'
+	foreach($m as $i) {
+		
+		$ti = [];
+		$ti_str = [];
+		$ti_tok = [];
+		
+		$comma = (is_null($i["preparation"]) | trim($i["preparation"]) == "") ? "" : ",";
+		
+		if (!is_null($i["amount"])) {
+			$amt = convert_float_to_fraction($i["amount"]);
+			array_push($ti,$amt);
+
+			$tmp_str = explode(" ", $amt);
+			foreach ($tmp_str as $str) {
+				array_push($ti_str,$str);
+				array_push($ti_tok,"CC0");
+			}
+		}
+		
+		if (!is_null($i["short_name"]) & trim($i["short_name"]) != "") {
+			array_push($ti,$i["short_name"]);
+			
+			$tmp_str = explode(" ", $i["short_name"]);
+			foreach ($tmp_str as $str) {
+				array_push($ti_str,$str);
+				array_push($ti_tok,"CC1");
+			}
+		}
+		
+		if (!is_null($i["size"]) & trim($i["size"]) != "") {
+			array_push($ti,$i["size"]);
+			
+			$tmp_str = explode(" ", $i["size"]);
+			foreach ($tmp_str as $str) {
+				array_push($ti_str,$str);
+				array_push($ti_tok,"CC2");
+			}
+		}
+		
+		if (!is_null($i["singular_name"])) {
+			array_push($ti,$i["singular_name"].$comma);
+			
+			$tmp_str = explode(" ", urldecode($i["singular_name"].$comma));
+			foreach ($tmp_str as $str) {
+				array_push($ti_str,$str);
+				array_push($ti_tok,"CC3");
+			}
+		}
+		
+		if (!is_null($i["preparation"]) & trim($i["preparation"]) != "") {
+			array_push($ti,$i["preparation"]);
+			
+			$tmp_str = explode(" ", urldecode($i["preparation"]));
+			foreach ($tmp_str as $str) {
+				array_push($ti_str,$str);
+				array_push($ti_tok,"CC4");
+			}
+		}
+		
+		
+		
+		array_push($total,trim(implode(" ", $ti)));
+		array_push($t_str,implode(" ",$ti_str));
+		array_push($t_tok,implode(" ",$ti_tok));
+	}
 	
 	
 ?>
@@ -38,7 +110,20 @@
 	<head></head>
 	<body>
 		<p>
-			<?php echo urldecode(implode("<br>",$total)); ?>
+			<?php 
+				//echo urldecode(implode("<br>",$total)); 
+				echo urldecode(implode("<br>",$t_tok));
+				 
+			?>
 		</p>
 	</body>
 </html>
+
+
+
+
+
+
+
+
+
